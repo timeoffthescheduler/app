b1410278ba0cfaa726c14e3cf3c987dc84fdd0e5
Zafar <zafar@Zafar-Mac.local>
2020-04-12 04:13:14 +0500

    Styled components for Header

--- src/components/commons/headerComponent.js
+++ src/components/commons/headerComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import {
+  HeaderContainer,
+  LogoContainer,
+  NavigationBar,
+  WelcomeMessageContainer,
+  Logout
+} from './styles';
 import { getCookie } from '../../utils/cookies';
-
-import './header.css';
 
 class HeaderComponent extends Component {
 
@@ -14,26 +18,36 @@ class HeaderComponent extends Component {
 
   render() {
     return (
-      <div className='header-container'>
-        <Link to='/' className='logo'></Link>
+      <HeaderContainer>
+        <LogoContainer to='/'></LogoContainer>
         {(getCookie('role') === 'Admin')
         ?
-        <ul className='navbar'>
-          <li><Link className={(window.location.pathname.split('/')[2] === 'dashboard') ? 'navlink active' : 'navlink inactive'} to='/admin/dashboard'>Dashboard</Link></li>
-          <li><Link className={(window.location.pathname.split('/')[2] === 'list') ? 'navlink active' : 'navlink inactive'} to='/admin/list'>Employees</Link></li>
-          <li><Link className={(window.location.pathname.split('/')[2] === 'new') ? 'navlink active' : 'navlink inactive'} to='/admin/new'>Add Employee</Link></li>
-        </ul>
+        <NavigationBar>
+          <li>
+            <Link className={(window.location.pathname.split('/')[2] === 'dashboard') ? 'navlink active' : 'navlink inactive'} to='/admin/dashboard'>Dashboard</Link>
+          </li>
+          <li>
+            <Link className={(window.location.pathname.split('/')[2] === 'list') ? 'navlink active' : 'navlink inactive'} to='/admin/list'>Employees</Link>
+          </li>
+          <li>
+            <Link className={(window.location.pathname.split('/')[2] === 'new') ? 'navlink active' : 'navlink inactive'} to='/admin/new'>Add Employee</Link>
+          </li>
+        </NavigationBar>
         :
-        <ul className='navbar'>
-          <li><Link className={(window.location.pathname.split('/')[2] === 'home') ? 'navlink active' : 'navlink inactive'} to='/employee/home'>Home</Link></li>
-          <li><Link className={(window.location.pathname.split('/')[2] === 'profile') ? 'navlink active' : 'navlink inactive'} to='/employee/profile'>Profile</Link></li>
-        </ul>
+        <NavigationBar>
+          <li>
+            <Link className={(window.location.pathname.split('/')[2] === 'home') ? 'navlink active' : 'navlink inactive'} to='/employee/home'>Home</Link>
+          </li>
+          <li>
+            <Link className={(window.location.pathname.split('/')[2] === 'profile') ? 'navlink active' : 'navlink inactive'} to='/employee/profile'>Profile</Link>
+          </li>
+        </NavigationBar>
         }
         {(getCookie('role') === 'Admin')
-        ? <span className='welcome uppercase'>Welcome <Link to='/admin/profile' className='link success'>Admin</Link></span> 
-        : <span className='welcome uppercase'>Welcome <Link to='/employee/profile' className='link success'>{getCookie('name')}</Link></span>}
-        <Link to='/logout' className='btn danger logout'>Logout</Link>
-      </div>
+        ? <WelcomeMessageContainer>Welcome <Link to='/admin/profile' className='link success'>Admin</Link></WelcomeMessageContainer> 
+        : <WelcomeMessageContainer>Welcome <Link to='/employee/profile' className='link success'>{getCookie('name')}</Link></WelcomeMessageContainer>}
+        <Logout to='/logout'  className='btn danger logout'>Logout</Logout>
+      </HeaderContainer>
     );
   }
 }
