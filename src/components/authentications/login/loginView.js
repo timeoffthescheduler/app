df13542bd1df701f3c29f1c7c2901644126d885e
Zafar <zafar@Zafar-Mac.local>
2020-04-11 10:33:05 +0500

    authentication area migrated to styled components

--- src/components/authentications/login/loginView.js
+++ src/components/authentications/login/loginView.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { checkCookie } from '../../../utils/cookies';
 import Button from '../../commons/buttons/button';
+import { AuthContainer, LogoContainer, AuthForm } from '../assets/guest-styles';
+import { Forgot } from './styles';
 
-import './login.css';
+import './login.js';
 
 const LoginView = (props) => (
-  <div className='login-container'>
-    <Link to='/' className='logo-login'></Link>
+  <AuthContainer>
+    <LogoContainer to="/"><img src="../assets/images/guest-logo.png" alt="Timeoff" /></LogoContainer>
     {(checkCookie() !== null) ? <Redirect to='/admin/dashboard' /> : null}
     {(!props.isSuccess) ? <div className='error'>{props.message}</div> : null}
-    <form onSubmit={props.handleLogin} className='login-form'>
+    <AuthForm onSubmit={props.handleLogin}>
       <div>
         <input type="text" name="username" placeholder="Username" className="fields" />
       </div>
@@ -20,11 +22,11 @@ const LoginView = (props) => (
       <div>
         <Button classes='btn success' buttonLabel='Save' />
       </div>
-      <p className='forgot-password'>
-         <Link to='forgot' className='link success'> Forgot your password?</Link>
+      <p>
+        <Forgot to='forgot' className='link success'>Forgot your password?</Forgot>
       </p>
-    </form>
-  </div>
+    </AuthForm>
+  </AuthContainer>
 );
 
 export default LoginView;
