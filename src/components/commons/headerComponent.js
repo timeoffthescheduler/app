8c34da7f766f16fd49843bc08aa39f93d3edb237
Zafar <zafar@Zafar-Mac.local>
2020-04-12 04:17:03 +0500

    minor improvements

--- src/components/commons/headerComponent.js
+++ src/components/commons/headerComponent.js
@@ -24,28 +24,47 @@ class HeaderComponent extends Component {
         ?
         <NavigationBar>
           <li>
-            <Link className={(window.location.pathname.split('/')[2] === 'dashboard') ? 'navlink active' : 'navlink inactive'} to='/admin/dashboard'>Dashboard</Link>
+            <Link
+              className={(window.location.pathname.split('/')[2] === 'dashboard')
+              ? 'navlink active' : 'navlink inactive'}
+              to='/admin/dashboard'>Dashboard</Link>
           </li>
           <li>
-            <Link className={(window.location.pathname.split('/')[2] === 'list') ? 'navlink active' : 'navlink inactive'} to='/admin/list'>Employees</Link>
+            <Link
+              className={(window.location.pathname.split('/')[2] === 'list')
+              ? 'navlink active' : 'navlink inactive'}
+              to='/admin/list'>Employees</Link>
           </li>
           <li>
-            <Link className={(window.location.pathname.split('/')[2] === 'new') ? 'navlink active' : 'navlink inactive'} to='/admin/new'>Add Employee</Link>
+            <Link
+              className={(window.location.pathname.split('/')[2] === 'new')
+              ? 'navlink active' : 'navlink inactive'}
+              to='/admin/new'>Add Employee</Link>
           </li>
         </NavigationBar>
         :
         <NavigationBar>
           <li>
-            <Link className={(window.location.pathname.split('/')[2] === 'home') ? 'navlink active' : 'navlink inactive'} to='/employee/home'>Home</Link>
+            <Link
+              className={(window.location.pathname.split('/')[2] === 'home')
+              ? 'navlink active' : 'navlink inactive'}
+              to='/employee/home'>Home</Link>
           </li>
           <li>
-            <Link className={(window.location.pathname.split('/')[2] === 'profile') ? 'navlink active' : 'navlink inactive'} to='/employee/profile'>Profile</Link>
+            <Link
+              className={(window.location.pathname.split('/')[2] === 'profile')
+              ? 'navlink active' : 'navlink inactive'}
+              to='/employee/profile'>Profile</Link>
           </li>
         </NavigationBar>
         }
         {(getCookie('role') === 'Admin')
-        ? <WelcomeMessageContainer>Welcome <Link to='/admin/profile' className='link success'>Admin</Link></WelcomeMessageContainer> 
-        : <WelcomeMessageContainer>Welcome <Link to='/employee/profile' className='link success'>{getCookie('name')}</Link></WelcomeMessageContainer>}
+        ? 
+        <WelcomeMessageContainer>Welcome <Link to='/admin/profile' className='link success'>Admin</Link>
+        </WelcomeMessageContainer> 
+        :
+        <WelcomeMessageContainer>Welcome <Link to='/employee/profile' className='link success'>{getCookie('name')}</Link>
+        </WelcomeMessageContainer>}
         <Logout to='/logout'  className='btn danger logout'>Logout</Logout>
       </HeaderContainer>
     );
